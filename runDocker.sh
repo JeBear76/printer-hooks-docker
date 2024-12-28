c872cc4147ca25f830fc10c832f16c54cfd8f1c0
Jebear76 <jerepondumie@gmail.com>
2024-12-28 10:11:08 +0200

    print-monitor build and run

--- runDocker.sh
+++ runDocker.sh
@@ -1,3 +1,3 @@
 docker build -t printer-webhooks .
 docker image prune -f
-docker run --rm -it -v ./audio:/usr/src/app/audio --mount type=bind,source=/mnt/wslg/PulseServer,target=/mnt/wslg/PulseServer -e HOST_NAME=$(hostname) -e PULSE_SERVER=unix:/mnt/wslg/PulseServer -e DEEPGRAM_API_KEY=$DEEPGRAM_API_KEY -p 80:5000 printer-webhooks
+docker run --rm -it -v ./audio:/usr/src/app/audio --mount type=bind,source=${XDG_RUNTIME_DIR}/pulse/native,target=${XDG_RUNTIME_DIR}/pulse/native -e HOST_NAME=$(hostname) -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -e DEEPGRAM_API_KEY=$DEEPGRAM_API_KEY -p 80:5000 printer-webhooks
