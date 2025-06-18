cfc3264763a4fd312c479f70f7b8bea607b45149
Jebear76 <jerepondumie@gmail.com>
2025-06-18 08:37:22 +0200

    trim EoF

--- runDocker.sh
+++ runDocker.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 docker build -t printer-webhooks .
 docker image prune -f
 docker run --rm -it -v ./audio:/usr/src/app/audio --mount type=bind,source=${XDG_RUNTIME_DIR}/pulse/native,target=${XDG_RUNTIME_DIR}/pulse/native -e HOST_NAME=$(hostname) -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -e DEEPGRAM_API_KEY=$DEEPGRAM_API_KEY -p 80:5000 printer-webhooks
